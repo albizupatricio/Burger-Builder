ab5e19418bd9cdefb37a951ecd86b50eb60c90e6
Patricio Albizu <albizupatricio@github.com>
2020-03-31 21:16:34 -0300

    Updates

---
+++ precache-manifest.51decb3d8ec48422e09e492ffc0c10fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c82d0a6131cc136623c088459c60375",
+    "url": "/Burger-Builder/index.html"
+  },
+  {
+    "revision": "56358dac1f4fa07356f8",
+    "url": "/Burger-Builder/static/css/main.61a34a90.chunk.css"
+  },
+  {
+    "revision": "9807660797c122f76e71",
+    "url": "/Burger-Builder/static/js/2.6eaf75b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Burger-Builder/static/js/2.6eaf75b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56358dac1f4fa07356f8",
+    "url": "/Burger-Builder/static/js/main.6ed3a601.chunk.js"
+  },
+  {
+    "revision": "d046763c52133da88796",
+    "url": "/Burger-Builder/static/js/runtime-main.f59328a7.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Builder/static/media/logo.b8503d26.png"
+  }
+]);
