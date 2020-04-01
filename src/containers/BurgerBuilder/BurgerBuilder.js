d84b2d80e69951da27197935166fd746f339b989
Patricio Albizu <albizupatricio@github.com>
2020-04-01 09:28:06 -0300

    Fix .json

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -114,7 +114,7 @@ class BurgerBuilder extends Component {
             },
             deliveryMethod: 'fastest'
         };
-        axios.post('/orders', order)
+        axios.post('/orders.json', order)
             .then(response => {
                 console.log(response);
             })
